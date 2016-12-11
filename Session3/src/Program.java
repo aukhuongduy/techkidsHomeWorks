0d9243222f240983e445af2bb62072aac17f538c
DESKTOP-M0ENNDH\Khuong Duy <duyakse04298@fpt.edu.vn>
2016-12-12 00:50:30 +0700

    session 3

---
+++ Session3/src/Program.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Khuong Duy on 12/5/2016.
+ */
+public class Program {
+    public static void main(String[] args){
+        GameWindow gameWindow = new GameWindow();
+        Thread thread = new Thread(gameWindow);
+        thread.start();
+    }
+
+}
